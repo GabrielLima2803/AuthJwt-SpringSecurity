99c095b02cdaf6801bbdcf1f46758f8d963c3f05
GabrielLima2803 <gabriellima2803@gmail.com>
2024-09-15 20:19:05 -0300

    delete: remoção de arquivos

--- src/main/java/dev/limasg/springsecurity/repository/TweetRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.limasg.springsecurity.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import dev.limasg.springsecurity.entities.Tweet;
-
-public interface TweetRepository extends JpaRepository <Tweet, Long> {
-
-}
